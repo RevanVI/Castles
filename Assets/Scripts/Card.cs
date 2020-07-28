9d979b5a157b1d27b5265eec5df1e52f011ed9c3
RevanVI <47805713+RevanVI@users.noreply.github.com>
2020-07-28 16:27:26 +0300

    initialize
    -card basis
    -cardproperties SO

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public enum EType
+    {
+        Common = 0,
+        Tower = 1,
+        Crow = 2,
+        Mage = 3,
+        Princess = 4,
+        Prince = 5,
+        King = 6,
+        Queen = 7,
+        Gun = 8,
+    }
+
+    private static SOCardProperties _cardProperties;
+
+    [SerializeField]
+    private Sprite _image;
+    [SerializeField]
+    private Sprite _backImage;
+
+    [SerializeField]
+    private GameObject _boardPlace;
+
+    private int _value;
+    public int Value
+    {
+        get
+        {
+            return _value;
+        }
+    }
+
+    private EType _type;
+    public EType Type
+    {
+        get
+        {
+            return _type;
+        }
+    }
+
+    public void SetData(int value)
+    {
+        _backImage = _cardProperties.BackImage;
+        int index = _cardProperties.SpecialCards.IndexOf(new SOCardProperties.SpecialCardData(value));
+        if (index != -1)
+        {
+            _image = _cardProperties.SpecialCards[index].Image;
+            _type = _cardProperties.SpecialCards[index].Type;
+        }
+        else
+        {
+            _image = _cardProperties.CommonImage;
+            _type = EType.Common;
+        }
+        _value = value;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SOCardProperties.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardProperties", menuName = "CardProperties", order = 1)]
+public class SOCardProperties : ScriptableObject
+{
+    public Sprite CommonImage;
+    public Sprite BackImage;
+
+    [SerializeField]
+    public List<SpecialCardData> SpecialCards;
+
+    [System.Serializable]
+    public class SpecialCardData: System.IComparable
+    {
+        public Sprite Image;
+        public int Value;
+        public Card.EType Type;
+
+        public SpecialCardData(int value)
+        {
+            Value = value;
+        }
+
+        public int CompareTo(object o)
+        {
+            SpecialCardData other = o as SpecialCardData;
+            if (other == null)
+                throw new System.Exception("object is not SpecialCardData class");
+            return Value.CompareTo(other.Value);
+        }
+    }
+}
